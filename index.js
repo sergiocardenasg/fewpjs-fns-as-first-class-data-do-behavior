d17465d51dda2b35a8c4432f982d55f65636151e
sergiocardenasg <sergiocardenas@utexas.edu>
2021-02-02 08:17:47 -0600

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,18 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  let hour = parseInt(time.split(":")[0]);
+  if (hour < 12) {
+    greeting = "Good Morning";
+  } else if (hour > 17) {
+    greeting = "Good Evening";
+  } else {
+    greeting = "Good Afternoon";
+  }
+  return greeting;
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(msg){
+  document.getElementById("greeting").innerText = msg;
+}
